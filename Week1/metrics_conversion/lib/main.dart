1fb9c5ef7b2a788dce2e8e1ecce1a7d8808bf581
Laxmi Kanth Oruganti <oruganti.laxmikanth@gmail.com>
2026-01-05 19:34:29 -0600

    Metric conversion  widget

--- Week1/metrics_conversion/lib/main.dart
+++ Week1/metrics_conversion/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:metrics_conversion/pages/metricconversion.dart';
 
 void main() {
   runApp(const MyApp());
@@ -30,7 +31,8 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page..'),
+      // home: const MyHomePage(title: 'Flutter Demo Home Page..'),
+      home: const MetricConversion(),
     );
   }
 }
---
+++ Week1/metrics_conversion/lib/pages/metricconversion.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MetricConversion extends StatelessWidget {
+  const MetricConversion({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Metric Conversion.."),
+        centerTitle: true,
+      ),
+    );
+  }
+}
