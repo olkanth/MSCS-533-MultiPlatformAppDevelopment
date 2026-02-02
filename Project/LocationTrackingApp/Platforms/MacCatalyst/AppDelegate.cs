3d02ca19f3a5545b03f5c2be60673734708a7bfc
Laxmi Kanth Oruganti <oruganti.laxmikanth@gmail.com>
2026-02-02 08:56:54 -0600

    Initial project commit

---
+++ Project/LocationTrackingApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace LocationTrackingApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
